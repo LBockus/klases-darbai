3a54ec39d69fd3358a8323f67973589f4bf90fa7
LBockus <bockus.lukas@gmail.com>
2022-12-12 16:10:09 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//task 1
+
+let a = 1;
+console.log('Kintamojo a reikšmė:', a);
+a = 2;
+console.log('Kintamojo a reikšmė:', a);
+
+//task 2
+
+let b = "Lukas Bočkus";
+let c = "Mano vardas ";
+console.log(c + b);
+
+//task 3
+
+let metai = 1997;
+let menuo = 3;
+let diena = 12;
+let suma = metai % 10 + menuo % 10 + diena % 10;
+console.log(suma);
