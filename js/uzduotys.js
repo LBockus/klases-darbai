3aaedb55a0be3e829cfa80b871f234f0cae87592
LBockus <bockus.lukas@gmail.com>
2022-12-15 13:03:19 +0200

    tasks 11-13 added, fixed task 5

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -29,15 +29,59 @@ let studentas = {
 
 // task 5
 
-let studentoMasyvas = ["Lukas", "Bockus", 1, "Code academy", [8, 7, 9, 6]];
+let studentuMasyvas = [
+    {
+        vardas: "Lukas",
+        pavarde: "Bočkus",
+        kursas: 1,
+        istaiga: "Code academy",
+        pazymiai: [8, 7, 9, 6, 10, 4]
+    },
+    {
+        vardas: "Lukas",
+        pavarde: "Bočkus",
+        kursas: 1,
+        istaiga: "Code academy",
+        pazymiai: [8, 7, 9, 6, 10, 4]
+    },
+    {
+        vardas: "Lukas",
+        pavarde: "Bočkus",
+        kursas: 1,
+        istaiga: "Code academy",
+        pazymiai: [8, 7, 9, 6, 10, 4]
+    },
+    {
+        vardas: "Lukas",
+        pavarde: "Bočkus",
+        kursas: 1,
+        istaiga: "Code academy",
+        pazymiai: [8, 7, 9, 6, 10, 4]
+    },
+    {
+        vardas: "Lukas",
+        pavarde: "Bočkus",
+        kursas: 1,
+        istaiga: "Code academy",
+        pazymiai: [8, 7, 9, 6, 10, 4]
+    },
+    {
+        vardas: "Lukas",
+        pavarde: "Bočkus",
+        kursas: 1,
+        istaiga: "Code academy",
+        pazymiai: [8, 7, 9, 6, 10, 4]
+    },
+
+];
 
 //task 6
 
-console.log(studentoMasyvas[4]);
+console.log(studentuMasyvas[4]);
 
 //task 7
 
-console.log(studentoMasyvas[0] + " " + studentas.pavarde + " mokosi" + studentoMasyvas[3] + ' ' + studentas.kursas + " kurse, jo pazymiai: " + studentoMasyvas[4]);
+console.log(studentuMasyvas[4].vardas + " " + studentuMasyvas[4].pavarde + " mokosi" + studentuMasyvas[4].istaiga + ' ' + studentuMasyvas[4].kursas + " kurse, jo pazymiai: " + studentuMasyvas[4].pazymiai);
 
 //task 8
 
@@ -62,3 +106,34 @@ for(let i = 0; i < studentas.pazymiai.length;i++)
 console.log('Pasirinktu pazymiu suma: ' + suma);
 
 //task 11
+
+function randomNumber() {
+    return Math.floor(Math.random() * studentas.pazymiai.length);
+}
+
+console.log('Dvieju atsitiktiniu pazymiu sandauga: ' + studentas.pazymiai[randomNumber()] * studentas.pazymiai[randomNumber()]);
+
+//task 12
+
+let tikrinimas = (typeof kursas === 'number');
+if(tikrinimas) {
+    console.log('Kursas yra number');
+}
+else {
+    console.log('Kursas nera number');
+}
+
+//task 13
+function kursoTikrinimas(studentas) {
+    if(studentas.kursas === 4 && typeof studentas.kursas === 'number') {
+        return ('Studentas ' + studentas.vardas + ' ' + studentas.pavarde + ' yra abiturientas.');
+    }
+    else {
+        return('Studentui ' + studentas.vardas + ' ' + studentas.pavarde + ' dar toli iki mokslu baigimo.');
+    }
+}
+console.log(kursoTikrinimas(studentuMasyvas[0]));
+
+//task 14
+
+
